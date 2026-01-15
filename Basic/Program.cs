d514483cfd928b457d588f5f54fb199194552829
JuanfeP2004 <juanf.ramirezc@upb.edu.co>
2026-01-15 18:11:12 -0500

    Added Exercise: Zigzag conversion

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -1,19 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.ComponentModel;
+
 try
 {
     Console.WriteLine("Basic Problems");
 
     Console.WriteLine("Problem #1: Find the longest substring");
-    string s = "pwwkew";
-    int substring = LengthOfLongestSubstring(s);
+    string example1 = "pwwkew";
+    int substring = LengthOfLongestSubstring(example1);
     Console.WriteLine($"The longest substring is {substring} caracters long");
 
     Console.WriteLine("Problem #2: Find the longest palindromic substring");
-    string t = "cbbd";
-    string substring_2 = LongestPalindrome(t);
+    string example2 = "cbbd";
+    string substring_2 = LongestPalindrome(example2);
     Console.WriteLine($"The longest palindromic substring is {substring_2}");
+
+    Console.WriteLine("Problem #3: Zigzag conversion");
+    string example3 = "PAYPAL";
+    string result3 = Convert(example3, 3);
+    Console.WriteLine($"The zigzag string is read: {result3}");
 }
 catch(Exception e)
 {
@@ -114,4 +121,75 @@ string LongestPalindrome(string s)
     }
 
     return return_s;
+}
+
+/*
+    Problem #3: Zigzag conversion
+    (https://leetcode.com/problems/zigzag-conversion/description/)
+*/
+string Convert(string s, int numRows) {
+    
+    if(s.Length < 1 || s.Length > 1000)
+        throw new Exception("String is too long or too short");
+    if(numRows < 1 || numRows > 1000)
+        throw new Exception("Number of rows is too long or too short");
+    if(numRows > s.Length)
+        throw new Exception("Number of rows is greater than the string");
+
+    string[] rows = new string[numRows];
+    int row = 0;
+
+    if(numRows == 1)
+        return s;
+    else if(numRows == 2)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            if(i%2 != 0)
+                rows[1] += s[i];
+            else
+                rows[0] += s[i]; 
+        }
+    }
+    else
+    {
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            row = i % (2*numRows - 2);
+
+            if(row < numRows)
+            {
+                rows[row] += s[i];
+            }
+            else
+            {
+                row = 2*numRows - (i % (numRows + (numRows - 2))) - 2;
+                for(int j = 0; j < rows.Length; j++)
+                {
+                    if (j == row)
+                    {
+                        rows[row] += s[i];
+                    }
+                    else
+                    {
+                        rows[j] += " ";
+                    }
+                }
+            }
+        }
+    }
+
+    string zigzagString = "";
+
+    foreach(string t in rows)
+    {
+        foreach(char c in t)
+        {
+            if(c != ' ')
+                zigzagString += c;
+        }
+    }
+
+    return zigzagString;
 }
